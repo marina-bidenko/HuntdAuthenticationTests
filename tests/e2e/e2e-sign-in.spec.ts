fe3e4ac3df373ea7fcc06b91fe6f8ae681c2a1f4
Marina Bidenko <bidenko.bmw@gmail.com>
2023-07-07 20:22:25 +0300

    Create possiteve sign in casese, negative with logged out user, negative with wrong credentials

---
+++ tests/e2e/e2e-sign-in.spec.ts
@@ -0,0 +1,113 @@
+import { test, expect, Page } from '@playwright/test';
+import { SignInPage } from '../../page-objects/authorization/SignInPage';
+import {
+  HomePageLoggedIn,
+} from '../../page-objects/HomePage';
+import {
+  goToSignInPage,
+  gotToSignUpPage,
+} from '../../helpers/login';
+
+const faker = require('faker');
+const config = require('../../config');
+
+test.describe('Logged in', () => {
+   let signInPage: SignInPage;
+
+  test.beforeEach(async ({ page }) => {
+    signInPage = await goToSignInPage(page);
+  });
+
+  test.describe.only('Negative casse for registered user', () => {
+    let page: Page;
+    let email: string;
+    let password: string;
+
+    test.beforeAll(async ({ browser }) => {
+      page = await browser.newPage();
+
+      const signUpPage = await gotToSignUpPage(page);
+
+      email = faker.internet.email();
+      password = faker.internet.password();
+
+      await signUpPage.login(email, password);
+      await page.waitForURL(config.BaseUrl + 'choose-profile');
+
+      const homePageIn = new HomePageLoggedIn(page);
+      await homePageIn.visit();
+      await homePageIn.logOut();
+
+      await page.close();
+    });
+
+    test('Error message when password is wrong', async ({ page }) => {
+      let wrongPassword = faker.internet.password();
+      while (wrongPassword === password) {
+        wrongPassword = faker.internet.password();
+      }
+
+      await signInPage.login(email, wrongPassword);
+      await page.waitForTimeout(300);
+      await signInPage.assertEmailErrorMessage('Wrong credentials');
+    });
+
+    test('Error message when password is empty', async ({ page }) => {
+      await signInPage.login(email, '');
+      await page.waitForTimeout(300);
+      await signInPage.assertPassordErrorMessage('Password is required');
+    });
+  });
+
+  test.describe('Negative cases for NOT registered user', () => {
+    let password: string;
+    let email: string;
+
+    test('Error message when email is not registered', async ({ page }) => {
+      password = faker.internet.password();
+      email = faker.internet.email();
+
+      await signInPage.login(email, password);
+      await page.waitForTimeout(300);
+      await signInPage.assertEmailErrorMessage('Wrong credentials');
+    });
+
+    test('Error message when email is empty', async ({ page }) => {
+      password = faker.internet.email();
+
+      await signInPage.login('', password);
+      await page.waitForTimeout(300);
+      await signInPage.assertEmailErrorMessage('Email is required');
+    });
+  });
+
+  test.describe('Positive cases', () => {
+    let page: Page;
+    let password: string;
+    let email: string;
+
+    test.beforeAll(async ({ browser }) => {
+      page = await browser.newPage();
+
+      const signUpPage = await gotToSignUpPage(page);
+
+      email = faker.internet.email();
+      password = faker.internet.password();
+
+      await signUpPage.login(email, password);
+      await page.waitForURL(config.BaseUrl + 'choose-profile');
+
+      const homePageIn = new HomePageLoggedIn(page);
+      await homePageIn.visit();
+      await homePageIn.logOut();
+
+      await page.close();
+    });
+
+    test('Singn in when user is registered', async ({ page }) => {
+      await signInPage.login(email, password);
+      await page.waitForURL(config.BaseUrl + 'choose-profile');
+      expect(page).toHaveURL(config.BaseUrl + 'choose-profile');
+    });
+  });
+});
