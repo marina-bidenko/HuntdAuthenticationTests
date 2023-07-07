4c577950a4399dbd2e8b984255f81abfbc922ee6
Marina Bidenko <bidenko.bmw@gmail.com>
2023-07-07 13:19:23 +0300

    Create BaseAuth page class with email, password input & errors  external auth links & submit button

---
+++ page-objects/authorization/BaseAuthorizationPage.ts
@@ -0,0 +1,46 @@
+import { Page, Locator, expect } from '@playwright/test';
+import { BasicPage } from '../BasicPage';
+
+export default abstract class BaseAuthPage extends BasicPage {
+  readonly emailInput: Locator;
+  readonly passwordInput: Locator;
+  readonly submitSingInButton: Locator;
+
+  readonly emailError: Locator;
+  readonly passwordError: Locator;
+
+  readonly googleButton: Locator;
+  readonly linkedInButton: Locator;
+  readonly gitHubButton: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.emailInput = this.page.locator('#email');
+    this.passwordInput = this.page.locator('#password');
+    this.submitSingInButton = this.page.locator('form > button');
+
+    this.emailError = this.page.$$('.mt-4')[0];
+    this.passwordError = this.page.locator('.mt-4')[1];
+
+    this.googleButton = this.page.locator('.icon-google');
+    this.linkedInButton = this.page.locator('.icon-linkedin');
+    this.gitHubButton = this.page.locator('.icon-github');
+  }
+
+  async typeCredentials(email: string, password: string) {
+    await this.emailInput.type(email);
+    await this.passwordInput.type(password);
+  }
+
+  async submitClick() {
+    await this.submitSingInButton.click()
+  }
+
+  async assertEmailErrorMessage(errorMessage: string) {
+    expect(this.emailError).toContainText(errorMessage);
+  }
+
+  async assertPassordErrorMessage(errorMessage: string) {
+    expect(this.passwordError).toContainText(errorMessage);
+  }
+}
