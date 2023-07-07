30e658a61be43abc82bf6810104b34b53aa3325c
Marina Bidenko <bidenko.bmw@gmail.com>
2023-07-07 16:43:01 +0300

    create test sign-in Create tests with emptyp fields create tests with invalid data create tests with doesent mathc data

---
+++ tests/e2e/e2e-sign-up.spec.ts
@@ -0,0 +1,99 @@
+import { test, expect } from '@playwright/test';
+import { SignUpPage } from '../../page-objects/authorization/SignUpPage';
+import { HomePageLoggedOut } from '../../page-objects/HomePage';
+
+const faker = require('faker');
+
+test.describe('Account creation', () => {
+  let homePage: HomePageLoggedOut;
+  let signUpPage: SignUpPage;
+  let email: string;
+  let password: string;
+
+  test.beforeEach(async ({ page }) => {
+    homePage = new HomePageLoggedOut(page);
+    await homePage.visit();
+    await homePage.clickOnSignUp();
+    signUpPage = new SignUpPage(page);
+
+    email = faker.internet.email();
+    password = faker.internet.password();
+  });
+
+  test.describe('Negative cases', () => {
+    test('Error message when email is empty', async ({ page }) => {
+      await signUpPage.login('', password);
+
+      await page.waitForTimeout(300);
+      await signUpPage.assertEmailErrorMessage('Email is required');
+    });
+
+    /** helo<@google.com test failed can be a bug */
+    ['hello', 'hello.com', 'h@g.c', 'helo<@google.com'].forEach((emailCase) => {
+      test(`Error message when email is ${emailCase}`, async ({ page }) => {
+        await signUpPage.login(emailCase, password);
+        await page.waitForTimeout(300);
+        await signUpPage.assertEmailErrorMessage('Wrong email');
+      });
+    });
+
+    test('Error message when password is empty', async ({ page }) => {
+      await signUpPage.login(email, '', password);
+      await page.waitForTimeout(300);
+      await signUpPage.assertPassordErrorMessage('Password is required');
+    });
+
+    test('Error message when repeat password is empty', async ({ page }) => {
+      await signUpPage.login(email, password, '');
+      await page.waitForTimeout(300);
+      await signUpPage.assertRepetPasswordErrorMessage(
+        'Please repeat your password',
+      );
+    });
+
+    // No validation on the site, so tests will fail
+    [
+      '12345aB',
+      '12345678',
+      'qwertyuI',
+      '123456AB',
+      '123456ab',
+      '12345aB/',
+    ].forEach(async (passwordCase) => {
+      test.skip(`Error message when password is ${passwordCase}`, async ({
+        page,
+      }) => {
+        await signUpPage.login(email, passwordCase);
+        await page.waitForTimeout(300);
+        await signUpPage.assertPassordErrorMessage('Wrong password');
+      });
+    });
+
+    test('Error message when repeate password doesent match with password', async ({
+      page,
+    }) => {
+      let password2 = faker.internet.password();
+      while (password == password2) {
+        password2 = faker.internet.password();
+      }
+
+      await signUpPage.login(email, password, password2);
+      await page.waitForTimeout(300);
+      await signUpPage.assertRepetPasswordErrorMessage(
+        'Please make sure your passwords match',
+      );
+    });
+  });
+
+  test.describe('Possitive cases', () => {
+    test.skip('Shoud sign up with valic cred', async ({ page }) => {
+      const email = await faker.internet.email();
+      const password = await faker.internet.password();
+
+      await signUpPage.login(email, password);
+      await page.waitForTimeout(3000);
+
+      expect(page).toHaveURL('https://huntd.tech/choose-profile');
+    });
+  });
+});
