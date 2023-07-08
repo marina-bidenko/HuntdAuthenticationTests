6b8df856627965687872027c71b1eace6ffd2e4e
Marina Bidenko <bidenko.bmw@gmail.com>
2023-07-08 10:15:10 +0300

    Create test configuration

---
+++ playwright.config.ts
@@ -0,0 +1,35 @@
+import { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  testDir: 'tests/e2e',
+  timeout: 60000,
+  retries: 1,
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 10000,
+    ignoreHTTPSErrors: true,
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+  },
+  reporter: [
+    ['line'],
+    ['allure-playwright']
+  ],
+  projects: [
+    {
+      name: 'Chromium',
+      use: { browserName: 'chromium' },
+    },
+    {
+      name: 'Firefox',
+      use: { browserName: 'firefox' },
+    },
+    {
+      name: 'Webkit',
+      use: { browserName: 'webkit' },
+    },
+  ],
+};
+
+export default config;
